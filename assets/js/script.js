d134bc0222bebf2dd382cc7f7e136486ccf43597
Jim Harris <arcangyl@gmail.com>
2021-04-09 23:50:48 -0400

    Added script functionality for the navbar buttons; made the navbar sticky; added functionality for the project displays on click of section header; show and hide the bio and work area functions added; navbar buttons CSS controls added to the script; cleaned up the CSS styles overall and added media queries for the mobile appearance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,58 @@
+
 $(document).ready (function () {
+    var stickyNavTop = $('.topnav').offset().top;
+    var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > stickyNavTop) {
+            $('.topnav').addClass('sticky');
+        } else {
+            $('.topnav').removeClass('sticky');
+        }
+    };
+    stickyNav();
+    $(window).scroll(function() {
+        stickyNav();
+    });
     $('#btnJS').click(function(event){
-        console.log('I was clicked');
         $('#jscript').toggle();
     });
+    $('#btnHC').click(function(event){
+        $('#htmlCSS').toggle();
+    });
+    $('#btnProj').click(function(event){
+        $('#projects').toggle();
+    });
     $('#aboutBtn').click(function () {
-        $('#bio').toggle();
-        $('#work').toggle();
+        $('#bio').show();
+        $('#work').hide();
+        $(this).css('background-color', '#101d94');
+        $('#workBtn').css('background-color', '');
+        $('#contactBtn').css('background-color', '');
+        $('#resumeBtn').css('background-color', '');
+    });
+    $('#workBtn').click(function () {
+        $('#bio').hide();
+        $('#work').show();
+       $(this).css('background-color', '#101d94');
+        $('#aboutBtn').css('background-color', '');
+        $('#contactBtn').css('background-color', '');
+        $('#resumeBtn').css('background-color', '');
+    });
+    $('#contactBtn').click(function () {
+        $('#bio').hide();
+        $('#work').show();
+       $(this).css('background-color', '#101d94');
+        $('#aboutBtn').css('background-color', '');
+        $('#workBtn').css('background-color', '');
+        $('#resumeBtn').css('background-color', '');
+    });
+    $('#resumeBtn').click(function () {
+        $('#bio').hide();
+        $('#work').show();
+       $(this).css('background-color', '#101d94');
+        $('#aboutBtn').css('background-color', '');
+        $('#workBtn').css('background-color', '');
+        $('#contactBtn').css('background-color', '');
     });
 })
 
